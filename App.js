028368e08eedc5e59a33726cbe27f1be4c1546ad
Javasamurai <szsimplesyed93@gmail.com>
2019-08-13 22:10:30 +0530

    Modified list

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import { ScrollView, Animated, Image, Platform, StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, Animated, Image, Platform, StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Preesssss Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Do tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
 
 class PokemonList extends Component {
+  state = {
+    clicked: false,
+    baseColor: "#ecf0f1",
+    clickedColor: "#2980b9"
+  }
+
+  onClickPokemon(id) {
+    this.setState({
+      clicked: true
+    });
+    this.props.updateView(true)
+  }
   render() {
     return (
-    <View style= {styles.listItem}>
-      <Text style = {{flex: 1,fontSize: 22, color: 'black'}}>{this.props.index}</Text>
-      <Text style = {{flex: 1, fontSize: 22, color: 'black'}}>{this.props.name}</Text>
-      <Image source = {{uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ this.props.index +'.png'}} style= {{flex: 1, width: 100, height: 100}}></Image>
+    <View style= {styles.listItem} onTouchStart= {()=>this.onClickPokemon(this.props.index)} onTouchEnd = {() =>this.setState({clicked: true})}>
+        <Text style = {{flex: 1, fontFamily: "Early-GameBoy", fontSize: 15, color: "white"}}>{this.props.index}. {this.props.name}</Text>
+      <Image source = {{uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ this.props.index +'.png'}} style= {{flex: 0.5, width: 100, height: 100}}></Image>
     </View>
     )
+  }
+}
+
+class PokemonInfo extends Component {
+  render() {
+    <View>
+      <Image source = {{uri:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'}} style= {{flex: 0.5, width: 100, height: 100}}></Image>
+    </View>
   }
 }
 
@@ -22,52 +37,75 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.spinValue = new Animated.Value(0);
-    this.state.pokemonresponse = [];
+    this.state.pokemonresponse = [{}];
   }
   state = {
-    pokemonresponse: []
+    pokemonresponse: [{}],
+    test: ""
   }
-  fetchPokemons() {
-    let that = this
-    fetch('https://pokeapi.co/api/v2/pokemon')
-    .then((response) => response.json())
-    .then((responseJSON) => {
-      that.setState({
-        pokemonresponse: responseJSON.results,
-        next: responseJSON.next
-      })
-    })
-  }
-  componentDidMount() {
-    this.fetchPokemons()
+  fetchPokemons(next) {
+    let that = this;
+
     Animated.timing(this.spinValue, {
       toValue: 1,
       duration: 500
     }).start()
+
+    fetch( next ? next : 'https://pokeapi.co/api/v2/pokemon')
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      let pokemonList = this.state.pokemonresponse;
+      
+      if(next) {
+        responseJSON.results.forEach(element => {
+          pokemonList.push(element)
+        });
+      }
+      that.setState({
+        pokemonresponse: pokemonList,
+        next: responseJSON.next
+      });
+    })
+  }
+  componentDidMount() {
+    this.fetchPokemons()
   }
   updateContent(evt) {
-    if(event.native.contentOffset.y) {
-      this.loadNext();
+    this.setState({
+      test: evt.nativeEvent.contentOffset.y + ":" + styles.listItem.height * this.state.pokemonresponse.length
+    })
+    if(evt.nativeEvent.contentOffset.y > (styles.listItem.height * this.state.pokemonresponse.length) / 2) {
+      this.fetchPokemons(this.state.next);
     }
+  }
+  updateView(info) {
+    this.setState({
+      info: info
+    })
   }
   render() {
     const spin = this.spinValue.interpolate({
       inputRange: [0, 1],
       outputRange: ["0deg", "360deg"]
-    })
+    });
+    if (this.state.info) {
+      return (<PokemonInfo/>)
+    } else {
+      return (
+        <View style = {styles.main_container}>
+          <View style ={{backgroundColor: '#FBD32C',alignItems: 'center', justifyContent: 'center', height: 120}}>
+            <Animated.Image source = {require('./pokeball.png')} style= {{ width: 100, height: 100, transform: [{rotate: spin}]}}/>
+          </View>
   
-    return (
-      <View style = {styles.main_container}>
-        <View style ={{backgroundColor: '#FBD32C',alignItems: 'center', justifyContent: 'center', height: 120}}>
-          <Animated.Image source = {require('./pokeball.png')} style= {{ width: 100, height: 100, transform: [{rotate: spin}]}}/>
+          {/* <Text>{this.state.test} dsfs</Text> */}
+          <ScrollView id= "sv" bounces= {true} bouncesZoom = {true} alwaysBounceVertical= {true} style = {{flex: 3, pagingEnabled: true}} onScroll={this.updateContent.bind(this)}>
+            {this.state.pokemonresponse.map((item, index) => (
+              <PokemonList item ={item.name} name = {item.name} index= {index + 1} key={index} updateView={()=> this.updateView}/>
+            ))}
+          </ScrollView>
         </View>
-        <ScrollView style = {{flex: 3, backgroundColor: 'black'}} style= {{pagingEnabled: true}} onScroll={this.updateContent}>
-          {this.state.pokemonresponse.map((item, index) => (
-            <PokemonList name = {item.name} index= {index + 1} key={index}/>
-          ))}
-        </ScrollView>
-      </View>
-    );
+      );
+    }
   }
 }
 
@@ -78,9 +116,13 @@ const styles = StyleSheet.create({
   listItem: {
     color: 'black',
     flexDirection: 'row',
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'flex-start'
+    paddingTop: 20,
+    paddingEnd: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    height: 125,
+    backgroundColor: '#34495e',
+    alignItems: 'center'
   },
   container: {
     flex: 1,
